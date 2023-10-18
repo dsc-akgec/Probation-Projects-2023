ce0ab4c5f2038f760f7a23d8528d459a650cb55a
Shorya <104529541+Shorya7@users.noreply.github.com>
2023-10-19 00:35:11 +0530

    Revert "Project Submission by Mohit Singh"

--- Mohit_Singh/route/route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const contact = require('../controler/appcontroller.js')
-const send = require('../controler/appcontroller.js')
-
-router.post('/contact', contact);
-
-module.exports = router;
